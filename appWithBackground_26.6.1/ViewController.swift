cf8d99cef0e4cefe6dcad8921ccf11ce35b0832c
ru5s <hellperock@gmail.com>
2022-08-21 11:16:37 +0500

    Initial Commit

---
+++ appWithBackground_26.6.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appWithBackground_26.6.1
+//
+//  Created by Ruslan Ismailov on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
